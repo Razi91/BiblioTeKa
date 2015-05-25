6fd8627421db8fddf7b35fed7334e7561d45fa3c
jkonieczny-koding <razi91@o2.pl>
2015-05-25 16:56:00 +0000

    formy

--- user/models.py
+++ user/models.py
@@ -1,39 +1,82 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
 from django.contrib.auth.models import User
 from django.db import models
 from datetime import datetime
-
-
+ 
+ 
 __author__ = 'jkonieczny'
-
-
+ 
+ 
 class Subscription(models.Model):
     name = models.CharField(max_length=32)
     initial = models.DecimalField(max_digits=5, decimal_places=2)
     monthly = models.DecimalField(max_digits=5, decimal_places=2)
-
-
+ 
+ 
 class ClientManager(models.Manager):
     def get_query(self):
         return super(ClientManager, self).select_related('User')
-
-
+ 
+ 
 class Client(models.Model):
     user = models.ForeignKey(User, null=False)
     birthday = models.DateField()
     pesel = models.CharField(max_length=11)
     credits = models.DecimalField(max_digits=5, decimal_places=2)
-
+ 
 class Staff(models.Model):
     user = models.ForeignKey(User, null=False)
-
-
+ 
+ 
 class SubscriptionActive(models.Model):
     client = models.ForeignKey('Client')
     subscription = models.ForeignKey('Subscription')
     begin = models.DateField(default=datetime.now)
     end = models.DateField()
-
-
+ 
+ 
 class Loan(models.Model):
     client = models.ForeignKey('Client')
     book = models.ForeignKey('books.BookEntity')
--- user/views.py
+++ user/views.py
@@ -1,12 +1,18 @@
 from books.models import *
+from user.models import *
+from user.forms import *
 
 __author__ = 'jkonieczny'
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseServerError
 
 def new_client(request):
-    books = BookTitle.data.all()
+    user_form = UserForm(prefix='user_')
+    client_form = ClientForm(prefix='client_')
     dict = {
-        'books': books
+        'forms': {
+            'user': user_form,
+            'client_form': client_form
+        }
     }
     return render(request, "user/new-client.html", dict)
